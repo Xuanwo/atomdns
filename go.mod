cf235a656b533958b429aabc502ee725e78a8694
Xuanwo <github@xuanwo.io>
2020-04-07 01:30:52 +0800

    server: Add cache support
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/hashicorp/hcl/v2 v2.3.0
 	github.com/miekg/dns v1.1.29
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 )
--- server/server.go
+++ server/server.go
@@ -3,8 +3,10 @@ package server
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/miekg/dns"
+	"github.com/patrickmn/go-cache"
 
 	"github.com/Xuanwo/atomdns/config"
 	"github.com/Xuanwo/atomdns/match"
@@ -17,11 +19,15 @@ type Server struct {
 	upsrteams map[string]upstream.Upstream // upstream_name -> upstream
 	matchers  []match.Match
 	rules     map[string]upstream.Upstream // match rules -> upstream
+
+	c *cache.Cache
 }
 
 // New will create a new dns server
 func New(cfg *config.Config) (s *Server, err error) {
-	s = &Server{}
+	s = &Server{
+		c: cache.New(600*time.Second, 30*time.Minute),
+	}
 
 	// Setup streams
 	s.upsrteams = make(map[string]upstream.Upstream)
@@ -55,18 +61,58 @@ func New(cfg *config.Config) (s *Server, err error) {
 func (s *Server) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	log.Print(r.Question)
 
-	req := &request.Request{R: r, W: w}
+	req := &request.Request{R: r}
+
+	if v, ok := s.c.Get(req.ID()); ok {
+		m := v.(*dns.Msg)
+		m.Id = r.Id
+		err := w.WriteMsg(m)
+		if err != nil {
+			log.Printf("write msg: %v", err)
+			return
+		}
+
+		log.Printf("cache hit for %s", req.ID())
+		return
+	}
+
+	var up upstream.Upstream
 
 	for _, m := range s.matchers {
 		if !m.IsMatch(req) {
 			continue
 		}
 		log.Printf("rule %s matched, served via %s", m.Name(), s.rules[m.Name()].Name())
-		s.rules[m.Name()].ServeDNS(req)
+
+		up = s.rules[m.Name()]
+		break
+	}
+	if up == nil {
+		log.Printf("no rules matched, served via %s", s.rules["default"].Name())
+
+		up = s.rules["default"]
+	}
+
+	m, err := up.ServeDNS(req)
+	if err != nil {
+		m = new(dns.Msg)
+		m.SetRcode(r, dns.RcodeServerFailure)
+	}
+
+	err = w.WriteMsg(m)
+	if err != nil {
+		log.Printf("dns response write failed: %v", err)
 		return
 	}
 
-	log.Printf("no rules matched, served via %s", s.rules["default"].Name())
-	s.rules["default"].ServeDNS(req)
+	if m.Rcode == dns.RcodeServerFailure {
+		return
+	}
+
+	if len(m.Answer) > 0 {
+		s.c.Set(req.ID(), m.Copy(), time.Duration(m.Answer[0].Header().Ttl)*time.Second)
+	} else {
+		s.c.Set(req.ID(), m.Copy(), 0)
+	}
 	return
 }
